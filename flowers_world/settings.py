a449d5836570a2f238f11e81e4b02b8c0177c1b6
rabi993 <rabi.21.pust@gmail.com>
2024-12-24 17:03:56 +0600

    chang

--- flowers_world/settings.py
+++ flowers_world/settings.py
@@ -72,7 +72,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'flowers_world.urls'
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['https://flower-world.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://flowers-world.onrender.com']
+
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5501",  # or the domain of your frontend
 ]
