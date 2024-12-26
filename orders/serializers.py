3542ad08464fe19eb930b5fd1a6471472716fad3
rabi993 <rabi.21.pust@gmail.com>
2024-12-26 09:05:39 +0600

    updated

--- orders/serializers.py
+++ orders/serializers.py
@@ -8,6 +8,12 @@ class OrderSerializer(serializers.ModelSerializer):
         model = models.Order
         fields = '__all__'
 
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+
 
 
 
