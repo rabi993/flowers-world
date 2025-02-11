643b13a3817a7f8a02cebb347910835b67a22fc2
rabi993 <rabi.21.pust@gmail.com>
2025-02-11 08:14:15 +0600

    update for vercel

--- flowers_world/settings.py
+++ flowers_world/settings.py
@@ -31,7 +31,8 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 CSRF_TRUSTED_ORIGINS = ['https://flowers-world-unkt.onrender.com','https://*.127.0.0.1']
 
 
@@ -72,10 +73,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'flowers_world.urls'
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['https://flowers-world-unkt.onrender.com']
+# CSRF_TRUSTED_ORIGINS = ['https://flowers-world-unkt.onrender.com']
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5501",
+    "http://127.0.0.1:5503",
     "https://flowers-world-unkt.onrender.com",  # or the domain of your frontend
 ]
 
@@ -95,16 +96,26 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'flowers_world.wsgi.application'
+WSGI_APPLICATION = 'flowers_world.wsgi.app'
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.vuzsdabytooalvrmqsgg',
+        'PASSWORD': 'i0FlDNI7fQly2Cia',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'PORT': '6543'
     }
 }
 
