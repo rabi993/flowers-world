29c82127d70377bf2ac4a522b97ace61cb7b7340
rabi993 <rabi.21.pust@gmail.com>
2025-02-12 09:17:09 +0600

    url correction

--- buyers/views.py
+++ buyers/views.py
@@ -56,9 +56,9 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect('https://flowers-world-two.vercel.app/login.html')
+        return redirect('https://rabi993.github.io/flowers-world-frontend-with-OnrenderAPI/login.html')
     else:
-        return redirect('https://flowers-world-two.vercel.app/registration.html')
+        return redirect('https://rabi993.github.io/flowers-world-frontend-with-OnrenderAPI/registration.html')
     
 
 class UserLoginApiView(APIView):
